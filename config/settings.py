d1394e1132f9b5ece1fb84ec3340eea217a20fb8
Divij Sharma <divijs75@gmail.com>
2024-06-17 03:31:39 +0530

    chore(db): added MySQL db
    Signed-off-by: Divij Sharma <divijs75@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     "corsheaders",
     "rest_framework",
 ]
@@ -84,8 +83,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "spapi",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306"
     }
 }
 
